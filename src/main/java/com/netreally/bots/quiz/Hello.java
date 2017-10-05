b87d95f182598a57c44bf78b634e460c3a0aef4c
Binu K Idicula <binu.idicula@outlook.com>
2017-10-05 11:28:21 +0530

    initial version

---
+++ src/main/java/com/netreally/bots/quiz/Hello.java
@@ -0,0 +1,14 @@
+package com.netreally.bots.quiz;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Integer, String> {
+
+
+    @Override
+    public String handleRequest(Integer myCount, Context context) {
+        return String.valueOf(myCount);
+    }
+
+}
